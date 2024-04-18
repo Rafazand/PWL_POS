7368d8c48ace84d653f6f879891d7733ed66ae1a
Rafazand <Shafarafazanda12@gmail.com>
2024-04-19 03:56:45 +0700

    Practicum-2.4

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -10,16 +10,41 @@ class UserController extends Controller
 {
     public function index()
     {
-        $data=[
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345')
-        ];
-        UserModel::create($data);
+        // $data=[
+        //     'level_id' => 2,
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345')
+        // ];
+        // UserModel::create($data);
 
+        // $user = UserModel::firstwhere('level_id', 1);
+        // $user = UserModel::where('level_id', 1)->first();
         // $user = UserModel::find(1);
-        $user = UserModel::all();
+        // $user = UserModel::all();
+        // $user = UserModel::findOr(20, ['username', 'nama'], function () {
+        //     abort(404);
+        // });
+
+        // $user = UserModel::findOrFail(1);
+        // $user = UserModel::where('username', 'manager9')->firstOrFail();
+        // $user = UserModel::where('level_id', 2)->count();
+        // dd($user);
+        // $user = UserModel::firstOrCreate(
+        //     [
+        //         'username' => 'manager',
+        //         'nama' => 'Manager'
+        //     ],
+        // );
+        $user = UserModel::firstOrCreate(
+            [
+                'username' => 'manager34',
+                'nama' => 'Manager Tiga Empat',
+                'password' => Hash::make('1234'),
+                'level_id' => 2
+            ],
+        );
+        $user->save();
         return view('user', ['data'=>$user]);
     }
 }
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -12,14 +12,14 @@
                 <th>Nama</th>
                 <th>ID Level Pengguna</th>
             </tr>
-            @foreach($data as $d)
+            {{-- @foreach($data as $d) --}}
             <tr>
-                <th>{{$d->user_id}}</th>
-                <th>{{$d->username}}</th>
-                <th>{{$d->nama}}</th>
-                <th>{{$d->level_id}}</th>
+                <td>{{$data->user_id}}</td>
+                <td>{{$data->username}}</td>
+                <td>{{$data->nama}}</td>
+                <td>{{$data->level_id}}</td>
             </tr>
-            @endforeach
+            {{-- @endforeach --}}
         </table>    
     </body>
 </html>
