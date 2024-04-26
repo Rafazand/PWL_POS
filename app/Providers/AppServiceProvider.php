c81a64eabf4c8dc364c7b3131cd1b2d0766a7774
Rafazand <Shafarafazanda12@gmail.com>
2024-04-26 16:32:55 +0700

    Jobsheet-5

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,24 +1,41 @@
 <?php
 
+// namespace App\Providers;
+
+// use Illuminate\Support\ServiceProvider;
+
+// class AppServiceProvider extends ServiceProvider
+// {
+//     /**
+//      * Register any application services.
+//      */
+//     public function register(): void
+//     {
+//         //
+//     }
+
+//     /**
+//      * Bootstrap any application services.
+//      */
+//     public function boot(): void
+//     {
+//         //
+//     }
+// }
+
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Yajra\DataTables\Html\Builder;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
-
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        Builder::useVite();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,10 @@ Route::get('/user/tambah',[UserController::class,'tambah'])->name('/user/tambah'
 Route::get('/user/ubah/{id}',[UserController::class,'ubah'])->name('/user/ubah');
 Route::get('/user/hapus{id}',[UserController::class,'hapus'])->name('/user/hapus');
 Route::get('/user/tambah_simpan',[UserController::class,'tambah_simpan'])->name('/user/tambah_simpan');
+Route::get('/kategori',[KategoriController::class,'index']);
+Route::get('/kategori/update/{id}',[KategoriController::class,'update'])->name('/kategori/update');
+Route::get('/kategori/delete/{id}',[KategoriController::class,'delete'])->name('/kategori/delete');
+
+
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
